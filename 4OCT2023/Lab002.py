3ca396623bc98a80f7a7f8609667d9da5e1dcee9
Rajeshpatra015 <147338378+Rajeshpatra015@users.noreply.github.com>
2023-10-08 23:03:47 +0530

    Add files via upload

---
+++ 4OCT2023/Lab002.py
@@ -0,0 +1,10 @@
+# print the name of the user by taking it from the input command.
+name = input("Enter your name")
+print(name)
+# Take first name and last name of user and print with sep.="-" and end with /t
+First_name = input("Enter your firstname")
+Last_name = input("Enter your lastname")
+print(First_name,Last_name,sep='-',end='\t')
+# Take a user input as name and say with message- you are welcome. "<pramod>" to the python class.
+name1 =input("Enter your name")
+print("you are welcome."+ name1 +" to the python class")
